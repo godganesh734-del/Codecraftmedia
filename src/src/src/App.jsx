19e0dc23605c720551aa60036eae210e246e3cef
godganesh734-del <godganesh734@gmail.com>
2026-08-22 23:39:13 +0530

    Create App.jsx

---
+++ src/src/src/App.jsx
@@ -0,0 +1,18 @@
+// ============================================================
+// File: App.jsx
+// ============================================================
+import BackgroundVideo from "./components/BackgroundVideo";
+import Navbar from "./components/Navbar";
+import Hero from "./components/Hero";
+
+function App() {
+  return (
+    <main className="relative bg-black h-screen w-screen flex flex-col overflow-hidden selection:bg-white selection:text-black shrink-0">
+      <BackgroundVideo />
+      <Navbar />
+      <Hero />
+    </main>
+  );
+}
+
+export default App;
